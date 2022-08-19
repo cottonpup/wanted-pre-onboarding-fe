b19bc181b48bd6630687598a5cb318f8f7438d7f
cottonpup <younggyoung.lee1@gmail.com>
2022-08-19 20:21:31 +0900

    Add login feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import AuthForm from './pages/AuthForm';
+import Todos from './pages/Todos';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.localStorage.setItem('access_token', token);
+    const savedToken = window.localStorage.getItem('access_token');
+    console.log(savedToken);
+    if (savedToken) {
+      navigate('/todo');
+    } else {
+      navigate('/');
+    }
+  }, [token, navigate]);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<AuthForm handleAuthToken={setToken} />} />
+        <Route path="/todo" element={<Todos />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Todos.jsx
@@ -0,0 +1,8 @@
+export default function Todos() {
+  return (
+    <>
+      <h1>TODOS</h1>
+      <h2>dasdasdasd</h2>
+    </>
+  );
+}
